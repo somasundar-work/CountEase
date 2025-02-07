20ff55144543b5894be8c79841f5c2af20055131
Somasundar S <somasundar.work@outlook.com>
2025-02-08 00:06:06 +0530

    Inital Project Creation

---
+++ src/CountEase/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CountEase
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/CountEase/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CountEase
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
